c1a435e4b367f407098a72aefa8dfae31d8725c7
Abyss <57658642+abyssxd@users.noreply.github.com>
2024-03-06 10:01:21 +0100

    Create nav.js

---
+++ js/nav.js
@@ -0,0 +1,4 @@
+    document.getElementById('mobile-menu').addEventListener('click', function () {
+        const navbarNav = document.querySelector('.navbar-nav');
+        navbarNav.classList.toggle('show');
+    });
